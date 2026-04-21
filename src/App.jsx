0da60876e21ab8c1b5d7ad178497cf12f754b4b8
leocodes-al <leonardo.sistemainformacao@gmail.com>
2026-04-21 20:33:39 -0300

    Adicionando exibição de informações climáticas

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { useState, useRef } from 'react'
 import axios from 'axios'
 import './App.css'
 import InforTempo from './components/InforTempo'
+import InforTempo5Dias from './components/InforTempo5Dias'
 
 function App() {
   const [tempo, setTempo] = useState('')
+  const [tempo_5_Dias, setTempo_5_Dias] = useState('')
+
   const inputRef = useRef()
 
   async function buscarCidade(){
@@ -12,8 +15,12 @@ function App() {
     const apiKey = 'd920742f127650dcc68334e06ed763a3'
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&lang=pt_br&units=metric`
+    const url_5_Dias = `https://api.openweathermap.org/data/2.5/forecast?q=${cidade}&appid=${apiKey}&lang=pt_br&units=metric`
 
     const dados = await axios.get(url)
+    const dados_5_Dias = await axios.get(url_5_Dias)
+
+    setTempo_5_Dias(dados_5_Dias.data)
     setTempo(dados.data)
 
   }
@@ -26,6 +33,7 @@ function App() {
 
       {/* se receber alguma informação, renderiza o componente para o InforTempo.jsx */}
       {tempo && <InforTempo tempo={tempo} />}
+      {tempo_5_Dias && <InforTempo5Dias tempo_5_Dias={tempo_5_Dias} />}
       
     </div>
   )
--- src/components/InforTempo.jsx
+++ src/components/InforTempo.jsx
@@ -1,17 +1,19 @@
+import './InforTempo.css';
+
 function InforTempo({ tempo }) {
 
   return (
-    <div>
+    <div className='container-tempo'>
       <h3>{tempo.name}</h3>
 
-      <div>
+      <div className='tempo-info'>
         <img src={`https://openweathermap.org/img/wn/${tempo.weather[0].icon}.png`} alt={tempo.weather[0].description}/>
         <p>{Math.round(tempo.main.temp)}°C</p>
       </div>
 
-      <p>{tempo.weather[0].description}</p>
+      <p className='descricao'>{tempo.weather[0].description}</p>
 
-      <div>
+      <div className='tempo-detalhes'>
         <p>Sensação Térmica: {Math.round(tempo.main.feels_like)}°C</p>
         <p>Umidade: {tempo.main.humidity}%</p>
         <p>Pressão: {tempo.main.pressure} hPa</p>
